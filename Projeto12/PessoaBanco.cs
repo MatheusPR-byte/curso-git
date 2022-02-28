6dc06cab0d53918b496788ad4672a9335b1cb26e
Matheus Alves <matheusgil29@hotmail.com>
2022-02-28 15:30:25 -0300

    Projeto iniciado com intenção de teste em Git

---
+++ Projeto12/PessoaBanco.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+namespace Projeto12 {
+    internal class PessoaBanco {
+
+        public int Conta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public PessoaBanco(int conta, string titular) {
+            Conta = conta;
+            Titular = titular;
+            Saldo = 0;
+        }
+        public PessoaBanco(int conta, string titular, double depositoInicial) : this(conta, titular) {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia) {
+            Saldo -= quantia + 5;
+        }
+
+        public override string ToString() {
+            return "Conta " 
+                + Conta
+                + ", Titular: " 
+                + Titular 
+                + ", Saldo: $ " 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Projeto12/Program.cs
@@ -0,0 +1,41 @@
+﻿using Projeto12;
+using System;
+using System.Globalization;
+class Program {
+    static void Main(string[] args) {
+
+        PessoaBanco conta;
+
+        Console.Write("Entre o número da conta: ");
+        int numero = int.Parse(Console.ReadLine());
+        Console.Write("Entre o titular da conta: ");
+        string titular = Console.ReadLine();
+        Console.Write("Haverá depósito inicial (s/n)? ");
+        char logica = char.Parse(Console.ReadLine());
+
+        if (logica == 's' || logica == 'S') {
+            Console.Write("Entre o valor de depósito inicial: ");
+            double depositoInicio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta = new PessoaBanco(numero, titular, depositoInicio);
+        }
+        else {
+            conta = new PessoaBanco(numero, titular);
+        }
+
+        Console.WriteLine("Dados da conta: ");
+        Console.WriteLine(conta);
+
+        Console.Write("Entre um valor para depósito: ");
+        double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        conta.Deposito(quantia);
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.WriteLine(conta);
+
+        Console.Write("Entre um valor para saque: ");
+        quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        conta.Saque(quantia);
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.WriteLine(conta);
+
+    }
+}
